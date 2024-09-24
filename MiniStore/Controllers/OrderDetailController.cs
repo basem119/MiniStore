29eca219eee327bc16e9196a6fe1ed1f21912c22
Basem Maher <basem.119maher@gmail.com>
2024-09-24 07:17:25 +0300

    photo

---
+++ MiniStore/Controllers/OrderDetailController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MiniStore.Data;
+using MiniStore.Models.Entities;
+using MiniStore.Models.UnitOfWork.UnitOfWork;
+using System.Security.Claims;
+
+namespace MiniStore.Controllers
+{
+    [Authorize(Roles = clsRole.roleAdmin )]
+    public class OrderDetailController : Controller
+    {
+
+        private readonly IUnitOfWork _orderDetail;
+        
+        public OrderDetailController(IUnitOfWork orderDetail)
+        {
+            _orderDetail = orderDetail;
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            return View( _orderDetail.orderDetails.Find(x=>x.Id == id));
+        }
+
+
+
+
+
+    }
+}
+
---
+++ MiniStore/Migrations/20240626161548_addTotalToOrder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiniStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class addTotalToOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 6, 26, 19, 15, 48, 258, DateTimeKind.Local).AddTicks(7950),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2023, 12, 8, 19, 21, 26, 394, DateTimeKind.Local).AddTicks(5336));
+
+            migrationBuilder.AddColumn<float>(
+                name: "Total",
+                table: "Orders",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Total",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2023, 12, 8, 19, 21, 26, 394, DateTimeKind.Local).AddTicks(5336),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 6, 26, 19, 15, 48, 258, DateTimeKind.Local).AddTicks(7950));
+        }
+    }
+}
